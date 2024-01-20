bbd97d905c7f4436f7069680e019be52fc7f5c56
ekuin0x <salhimouiz1@gmail.com>
2024-01-20 21:17:05 +0100

    gg

--- app.py
+++ app.py
@@ -86,19 +86,17 @@ def linkedin(state, keyword,code) :
 
 
 keywords = ["Administrative assistant", "Customer service","Retail","Finance","Graphic designer","Healthcare","Insurance", "management", "lawyer", "fitness", "seo", "sales", "doctor","ecommerce", "real estate agent"] 
-while True :
-    keyword = random.choice(keywords)
-    state = random.choice(list(states))
-
-    for i in range(4):
-        code = random.choice(list(states[state]))
-        t = threading.Thread(target=linkedin, args=(state, keyword, code,))
-        t.start()
-    sleep(25)
-
 
 @app.route("/alive")
 def alive():
+    keyword = random.choice(keywords)
+    state = random.choice(list(states))
+    for i in range(4):
+        print("started")
+        code = random.choice(list(states[state]))
+        t = threading.Thread(target=linkedin, args=(state, keyword, code,))
+        t.start()
+    sleep(1)
     return "hello World"
     
 
